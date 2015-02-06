ae3a9f192189afdd58820f81e5282f70274cd7f4
Kjell-Magne Øierud <kjellm@oierud.net>
2015-02-06 17:56:21 +0100

    Fix ex 1.8

--- 1-8.scm
+++ 1-8.scm
@@ -1,12 +1,21 @@
-(display "Ex 1.8:")
-(newline)
+(define tolerance 0.001)
 
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) tolerance))
+(define (cube x) (* x x x))
 
+(define (cube-root-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cube-root-iter (improve guess x) x)))
 
 (define (improve guess x)
-  (/ (+ (square (/ x guess)) (* 2 guess)) 3))
+  (/ (+ (/ x (square guess))
+        (* 2 guess))
+     3))
 
-(display (sqrt 4))
+(define (good-enough? guess x)
+  (< (abs (- (cube guess) x)) tolerance))
+
+(define (cube-root x) (cube-root-iter 1.0 x))
+
+(display (cube-root 27))
 (newline)
